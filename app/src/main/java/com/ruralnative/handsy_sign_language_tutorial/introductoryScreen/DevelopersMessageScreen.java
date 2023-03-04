3abd24069c65329157b7eb362f3988e989dc791d
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-03-04 19:07:15 +0800

    Creation of User Introduction Draft

---
+++ app/src/main/java/com/ruralnative/handsy_sign_language_tutorial/introductoryScreen/DevelopersMessageScreen.java
@@ -0,0 +1,16 @@
+package com.ruralnative.handsy_sign_language_tutorial.introductoryScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ruralnative.handsy_sign_language_tutorial.R;
+
+public class DevelopersMessageScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developers_message_screen);
+    }
+}
---
+++ app/src/main/java/com/ruralnative/handsy_sign_language_tutorial/introductoryScreen/UserIntroductionScreen.java
@@ -0,0 +1,16 @@
+package com.ruralnative.handsy_sign_language_tutorial.introductoryScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ruralnative.handsy_sign_language_tutorial.R;
+
+public class UserIntroductionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_introduction_screen);
+    }
+}
