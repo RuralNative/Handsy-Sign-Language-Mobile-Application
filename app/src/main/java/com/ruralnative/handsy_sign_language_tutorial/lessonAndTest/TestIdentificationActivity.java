75d54a205d43bf98c10b4141933243e33cf6e786
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-03-06 19:08:53 +0800

    Completed Lesson and Multiple Choice Test Screen

---
+++ app/src/main/java/com/ruralnative/handsy_sign_language_tutorial/lessonAndTest/TestIdentificationActivity.java
@@ -0,0 +1,16 @@
+package com.ruralnative.handsy_sign_language_tutorial.lessonAndTest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ruralnative.handsy_sign_language_tutorial.R;
+
+public class TestIdentificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identification_test);
+    }
+}
