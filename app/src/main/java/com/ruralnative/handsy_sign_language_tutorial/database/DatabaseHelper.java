2f36bb2237a864928487ac2292459d4b10a143df
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-03-13 15:08:34 +0800

    Write Code for Database Prepopulation
    Wrote methods necessary for the prepopulation of application database in the database helper, and method for prepopulation of database upon application initializaiton

--- app/src/main/java/com/ruralnative/handsy_sign_language_tutorial/database/DatabaseHelper.java
+++ app/src/main/java/com/ruralnative/handsy_sign_language_tutorial/database/DatabaseHelper.java
@@ -2,12 +2,22 @@ package com.ruralnative.handsy_sign_language_tutorial.database;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "handsy_database";
+    Context context;
+    private static final String DATABASE_NAME = "handsy_database.db";
     private static final int DATABASE_VERSION = 1;
     private static final String CREATE_TABLE_USER_INFORMATION = "CREATE TABLE user_information_table " +
             "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
@@ -58,5 +68,41 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         database.execSQL("DROP TABLE IF EXISTS multiple_choice_test_table");
         database.execSQL("DROP TABLE IF EXISTS identification_test_table");
         onCreate(database);
+    }
+    public void createDatabase() throws IOException {
+        boolean databaseExist = checkDatabaseExist();
+        if (!databaseExist) {
+            this.getWritableDatabase();
+
+            InputStream inputStream = context.getAssets().open(DATABASE_NAME);
+
+            OutputStream outputStream = Files.newOutputStream(Paths.get(getDatabasePath()));
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+        }
+    }
+
+    private boolean checkDatabaseExist() {
+        SQLiteDatabase checkDB = null;
+        try {
+            String path = getDatabasePath();
+            checkDB = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READWRITE);
+        } catch (SQLiteException e) {
+            // database does not exist
+        }
+        if (checkDB != null) {
+            checkDB.close();
+        }
+        return checkDB != null;
+    }
+
+    private String getDatabasePath() {
+        return context.getFilesDir().getPath() + File.separator + DATABASE_NAME;
     }
 }
--- app/src/main/java/com/ruralnative/handsy_sign_language_tutorial/mainScreen/MainScreen.java
+++ app/src/main/java/com/ruralnative/handsy_sign_language_tutorial/mainScreen/MainScreen.java
@@ -5,12 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ruralnative.handsy_sign_language_tutorial.R;
+import com.ruralnative.handsy_sign_language_tutorial.database.DatabaseHelper;
+
+import java.io.IOException;
 
 public class MainScreen extends AppCompatActivity {
+    private DatabaseHelper databaseHelper = new DatabaseHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        createDatabase();
     }
+
+    private void createDatabase() {
+        try {
+            databaseHelper.createDatabase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
